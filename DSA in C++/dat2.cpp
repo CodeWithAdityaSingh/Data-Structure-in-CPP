b68bad80dd755dc902cee424313524029216f7d9
Aditya Singh <adityasingh161020003@gmail.com>
2024-09-09 12:09:05 +0530

    Day3 solving various questions and understand how nseted loop Works⭐⭐

---
+++ DSA in C++/dat2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    // int  a = 9;
+    // double b = 6;
+    // cout << (3>5) << endl;
+    // cout << (a/ (double)6) << endl; // we also do directly
+    // cout << (3<=3 ) << endl;
+    //  cout <<(3+5) << endl;
+
+     // Sum of two Numbers 
+
+    //  int  c,d;
+    //  cout << "Enter c value : " << endl;
+    //  cin>>c;
+    //  cout << "Enter d value : " << endl;
+    //  cin>>d;
+    //  int sum = (c+d);
+    //  cout << " Sum of  A and B : " << sum << endl;
+
+    //  //  type conversion
+
+    //  int y = 98;
+    //  char f = y;
+
+    //  cout<< f <<endl;
+
+    // Increment and decrement
+
+    //pre Increment
+    // int a = 66;
+    //  int b = a++;
+     
+     
+    // cout << b << endl; 
+    // cout << a << endl; 
+
+
+
+
+return 0;
+}
+
+//Binary orerators + // a+b a is used between 2 variables or datatype like 7+8
+
+//Unary Operator (i++) used with only one operator or (i+1)
+
+// increment and decrement operator :- (++,--)
---
+++ DSA in C++/day3.cpp
@@ -0,0 +1,179 @@
+// Conditional Statement and Loop
+
+#include <iostream>
+using namespace std;
+
+int main(){
+
+// eligible for Vote
+// int age;
+
+// cout << "Enter Your Age : " ;
+// cin >> age;
+
+// if(age > 18 && age <80){
+//     cout << "You Can Vote";
+// }
+// else{
+//     cout << "You Can't Vote";
+// }
+
+// Find Number is positive or negative
+
+// int number;
+// cout << "Enter Number fot find positive or negative : ";
+// cin >> number;
+// if(number >= 0) cout << "Positive Number";
+
+// else cout << "Nagative Number";
+
+// Number is even or Odd
+
+// int Number;
+
+// cout << "Enter Number : ";
+// cin >> Number;
+
+// if(Number %2==0){
+
+//     cout << "Number is Even";
+
+// } else {
+//     cout << "Number is Odd";
+
+// }
+
+// Grade of Student
+
+// int Marks ;
+
+// cout << "Enter Marks of Student : ";
+// cin >> Marks;
+
+// if(Marks >=80 &&  Marks <=100){
+//     cout << "First Class With Distinction\n";
+// } else if(Marks >=60 && Marks <= 80) {
+//     cout << "First Class\n";
+// } else {
+//     cout << "Pass\n";
+// }
+
+// Number is upper Case or Lower Case
+// int Character;
+
+// cout << "Enter Charatcer  : ";
+// cin >> Character;
+
+//  cout << (Character ? "Charatcter is  True" :  "Character is false");
+
+
+// int number = 1;
+
+//  while(number <=5){
+
+//     cout << number<< endl;
+//     number++;
+//  }
+ 
+
+// for(int i=0 ; i <10; i++){
+
+//     cout << i << endl;
+//  }
+
+// return 0;
+
+// int sum = 0;
+// for(int i= 1 ; i <= 10; i++){
+   
+// sum+=i;
+ 
+// }
+
+// cout<< "Sum : = " << sum;
+
+// int sum =0;
+
+// int i=1;
+
+// while(i<=10){
+
+//     sum+=i;
+
+//     if(sum == 50) break;
+//      i++;
+// }
+
+// cout<< "sum : = " <<sum << endl; 
+
+// int sum =0;
+// for(int i=0 ; i <= 5; i++){
+//    if(i%2 ==1){
+
+//     sum+=i;
+//    }
+    
+// }
+
+// cout << "Sum : = " << sum;
+
+
+// int sum =0;
+// int i=0;
+// int n=10;
+
+// while(i<=n){
+
+//     if(i%2==1){
+
+//         sum+=i;
+//     }
+//     i++;
+// }
+
+// cout << sum << endl;
+// int a = 0;
+
+// do{
+//     cout << "Aditya\n";
+//      a++;
+// }while(a<10);
+
+// Number is prime or not
+
+
+// int number;
+// bool isPrime = true;
+
+// cout << "Enter Number";
+
+// cin>> number;
+
+// for(int i=2; i<number-1; i++){
+
+//     if(number % i == 0){
+
+//       isPrime = false;
+//       break;
+//     }
+
+// }
+// cout << (isPrime ?  "Prime Number" :  "Non Prime");
+
+
+for(int i=0 ; i <=5; i++){
+
+    for(int j=1; j<=5;j++){
+    
+      cout << "*";
+
+    }
+
+    cout << endl;
+}
+
+
+  
+  return 0;
+
+}
