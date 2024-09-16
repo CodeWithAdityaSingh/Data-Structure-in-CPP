bd24afcaf0480169cd26ccf1f55d1154f000b48c
Aditya Singh <adityasingh161020003@gmail.com>
2024-09-16 16:07:52 +0530

    Learning BinaryNumber system

--- DSA in C++/Day4BinaryNumbersysytem.cpp
+++ DSA in C++/Day4BinaryNumbersysytem.cpp
@@ -1,36 +1,79 @@
 #include <iostream>
 using namespace std;
 
-int BinaryToDecimal(int number){
-  int answer = 0;
 
-  int power=1;
-
-while(number > 0){
-
-    
-   int remainder = number%2;
-
-   number  = number/2;
-
-   answer += (remainder*power);
-
-   power = power*10;
-
-
-
-}
- return  answer;
-
-
-}
-
-int main(){
+// int decimalToBinary(int number){
    
 
-   cout << BinaryToDecimal(10);
+//    int sum =0,power = 1;
+
+//    while(number>0){
+
+       
+//        int remainder = number%2;
+
+//        number/=2;
+
+//        sum += remainder*power;
+
+//        power*=10;
 
 
-  
+//    }
+
+
+//    return sum;
+
+// }
+
+
+// int binaryToDecimal(int number){
+
+//    int sum = 0;
+
+//    int power = 1;
+
+
+//    while(number>0){
+
+//       int lastNumber = number%10;
+
+//       number/=10;
+
+//       sum += (lastNumber*power);
+
+//       power =  power*2;
+
+
+//    }
+
+
+//  return sum;
+
+// }
+
+
+
+int main(){
+
+
+   cout 
+
+
+   // cout << binaryToDecimal(101010) << endl;
+
+   // for(int i=1;i<=10;i++){
+
+   //    cout << decimalToBinary(i) << endl;
+   // }
+                             
+   // cout << decimalToBinary(10) << endl;;
+
+
+   cout << 100%10;
+
+
+
+
 return 0;
 }
